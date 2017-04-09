2cf9abe1db099378092bc74310004cf127c1931c
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-04-08 22:41:00 -0600

    user tokens

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ app.delete('/todos/:id', (req, res) => {
 })
 app.patch('/todos/:id',(req,res) => {
   var id = req.params.id;
-  var body = lodash._(req.body, ['text', 'completed']);
+  var body = pick._(req.body, ['text', 'completed']);
 
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
@@ -81,7 +81,22 @@ app.patch('/todos/:id',(req,res) => {
   }).catch((e) => {
     res.status(400).send();
   })
+})
 
+app.post('/users', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+
+
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 })
 
 app.listen(3000, () => {
