971631ea98628f557edb9e46ad8f47ea7b1104b8
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-04-08 23:09:23 -0600

    users middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -87,9 +88,6 @@ app.post('/users', (req,res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-
-
-
   user.save().then(() => {
     return user.generateAuthToken();
   }).then((token) => {
@@ -97,8 +95,27 @@ app.post('/users', (req,res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+// var authenticate = (req, res, next) => {
+//   var token = req.header('x-auth');
+//
+//   User.findByToken(token).then((user) => {
+//     if(!user) {
+//       return Promise.reject();
+//     }
+//     req.user = user;
+//     req.token;
+//     next();
+//   }).catch((e) => {
+//     res.status(401).send();
+//   });
+// }
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+
 })
 
+
 app.listen(3000, () => {
-  console.log('Started on on port 3000');
+  console.log(`Started on on port ${3000}`);
 })
