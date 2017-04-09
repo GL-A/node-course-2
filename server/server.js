3e545467cd94e7337562f6158ec0cc6bd2a639b6
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-04-08 18:05:40 -0600

    add get todos by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {User} = require('./models/user');
@@ -28,6 +29,20 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   })
 })
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
 
 app.listen(3000, () => {
   console.log('Started on on port 3000');
