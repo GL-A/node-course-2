4487e7085c2f5ed88124dc5852492e0af807aad5
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-04-16 15:40:13 -0600

    d

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc'
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashpassword = '$2a$10$U3i1CJbdkAOLMJhGFo0TFuARFaO1Vgzuz9TdYKHpwC8enTWhCrYsK';
+bcrypt.compare(password, hashpassword, (err, res) => {
+  console.log(res)
+})
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 // jwt.verify;
 
 // var message = "I am user number 3";
